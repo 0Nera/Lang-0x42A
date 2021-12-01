316ad1e9510805a98f9c72d22cb51285c8803eaf
Aren Elchinyn <slizen.can@gmail.com>
2021-12-01 21:22:26 +0300

    Initial commit

---
+++ app/src/main/java/com/nera/lang_0x42a/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nera.lang_0x42a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
